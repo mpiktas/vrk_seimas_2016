c2f5d9fc584523e81f413821fcab8fcc547eeded
Vaidotas Zemlys <zemlys@gmail.com>
2016-10-11 20:32:35 +0300

    Daugiamandaciu rezultatai

--- apygardu_lentos.R
+++ apygardu_lentos.R
@@ -18,9 +18,10 @@ format_main_table <- function(x) {
     res
 }
 
-get_data <- function(x) {
+
+get_data <- function(x, atag = "h3") {
     ff <- read_html(x, encoding = "utf-8")
-    nm <- ff %>% html_nodes("h3")  %>% html_text %>% grep("apygarda",.,value=TRUE) 
+    nm <- ff %>% html_nodes(atag)  %>% html_text %>% grep("apygarda",.,value=TRUE) 
     tbs <- ff %>% html_nodes("table") %>% html_table(fill=TRUE)
     names(tbs) <- c("Header","Balsai","Apylinkes")
     c(list(apygarda = nm),tbs)
@@ -58,4 +59,40 @@ apyll <- apygf %>% lapply(function(x)get_apyl_link(read_html(x, encoding="utf-8"
     do.call("c",.)
 
 writeLines(apyll,"link_data/apylinkiu_linkai.txt")
+
+dmapygf <- dir("html_data/dm_apygardos",full.names=TRUE)
+
+ff <- read_html(dmapygf[1], encoding = "utf-8")
+
+dmvapy <- dmapygf %>% lapply(get_data, atag="h2") 
+
+format_dm_main_table <- function(x) {
+    hd <- x[1,-3]
+    res<-x[-1,-3]
+    colnames(res)<-hd
+    res
+}
+combine_dm_info <- function(x) {
+    format_dm_main_table(x$Balsai) %>% mutate(Apygarda = x[["apygarda"]])
+}
+
+dtdmapy <- dmvapy %>% lapply(combine_dm_info) %>% bind_rows
+
+colnames(dtdmapy) <- c("partijos_no","partija","apylinkes","pastas","balsai","proc_rinkejai","proc_rinkejai_lt","apygarda")
+
+dm1 <- dtdmapy %>% filter(partijos_no != "")
+pnames2 <- read.csv2("csv_data/partiju_santrumpos2.csv", stringsAsFactors = FALSE)
+
+dm2 <- dm1 %>% separate(apygarda, c("apygarda", "Numeris"),sep="[(]")  %>% 
+    mutate(Numeris=gsub("Nr.","",Numeris)) %>% 
+    mutate(Numeris=as.integer(gsub(").*","",Numeris))) %>% 
+    arrange(Numeris) %>% rename(apygardos_no = Numeris) %>% 
+    mutate(apygarda = str_trim(apygarda)) %>% mutate(partijos_no=as.integer(partijos_no)) %>% 
+    inner_join(pnames2 %>% select(partijos_no,partija1), by = "partijos_no") %>% 
+    rename(partija_full = partija) %>% rename(partija = partija1)
+
+    
+
+
+dm2 %>% write.csv2(file="csv_data/daugiamandaciu_apygardu_rinkimu_rezultatai.csv", row.names= FALSE)
 
--- scrap.R
+++ scrap.R
@@ -15,3 +15,15 @@ fn <- apl1 %>% gsub(".*_rpgId-","",.)
 
 writeLines(apl1,"link_data/apygardu_linkai.txt")
 
+
+dap <- read_html("html_data/dm_apygardos.html", encoding="utf-8")
+
+dapl <- dap %>% html_nodes("table")
+
+dapl1 <- dapl[[3]] %>% html_nodes("a") %>% html_attrs() %>% 
+    lapply(function(x) {
+    names(x) <-NULL
+    paste0(link_pref,x)
+}) %>% do.call("c",.)
+
+writeLines(dapl1,"link_data/dm_apygardu_linkai.txt")
